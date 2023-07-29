adf84499deb269adc6bd23dc4791d62b0a3ed46b
SirotovAlexander <alexander.sirotov@gmail.com>
2023-07-29 13:52:11 +0300

    update
    update

--- src/api/getDataAPI.js
+++ src/api/getDataAPI.js
@@ -2,7 +2,10 @@ import axios from 'axios';
 // const API_KEY =
 // 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2ZGMzYzY4MGExNGI5ZWZiN2U5YjlhNWQ2ZmQzZGNlMyIsInN1YiI6IjY0YzNmNDE4MDI4ZjE0MDBlZTlkOTEwYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zlpBdQHE5iERq6cscX7pgpnknyuueyRqCcB7RBnCZlA';
 const BASE_LINK = 'https://api.themoviedb.org/3';
-const END_POINT = 'trending/movie/day?';
-const API_KEY = '6dc3c680a14b9efb7e9b9a5d6fd3dce3';
+const END_POINT = '/trending/movie/day?';
+const API_KEY = 'api_key=6dc3c680a14b9efb7e9b9a5d6fd3dce3';
 
-const getTrending = () => {};
+export const getTrending = async () => {
+  const response = await axios.get(`${BASE_LINK}${END_POINT}${API_KEY}`);
+  console.log(response);
+};
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,8 @@
+import { Route, Routes } from 'react-router-dom';
+import { getTrending } from '../api/getDataAPI';
+
 export const App = () => {
+  getTrending();
   return (
     <div
       style={{
@@ -7,7 +11,7 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
