3c7f4cc210e80bb0e3ff4723aee10e77fb48fc99
SirotovAlexander <alexander.sirotov@gmail.com>
2023-07-31 01:50:12 +0300

    done all tasks
    done all tasks

--- src/Layout/SharedLayout.jsx
+++ src/Layout/SharedLayout.jsx
@@ -1,11 +1,15 @@
-import Header from 'components/Header/Header';
+import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
+import Header from 'components/Header/Header';
 
 const SharedLayout = () => {
   return (
     <div>
       <Header />
-      <Outlet />
+      <Suspense>
+        {' '}
+        <Outlet />
+      </Suspense>
     </div>
   );
 };
