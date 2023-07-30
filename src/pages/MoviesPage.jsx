c7e10df3bdba6911608382168df761fed793e363
SirotovAlexander <alexander.sirotov@gmail.com>
2023-07-31 00:02:01 +0300

    Update MoviesPage.jsx

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,12 +1,21 @@
-import { Outlet, Link, useParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { getMovieBySearch } from '../api/getDataAPI';
 
 const MoviesPage = () => {
+  const [query, setQuery] = useState('');
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const onSubmit = event => {
+    event.preventDefault();
+    const { query } = event.currentTarget;
+    console.dir(query.value);
+  };
+
   return (
     <div>
-      <form>
-        <input type="text" autoComplete="off" />
+      <form onSubmit={onSubmit}>
+        <input type="text" name="query" autoComplete="off" />
         <button type="submit">Search</button>
       </form>
     </div>
