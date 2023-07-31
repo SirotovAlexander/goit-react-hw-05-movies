978ee5f2ecfaf807a03dd38d1bdee16383298c7a
SirotovAlexander <alexander.sirotov@gmail.com>
2023-07-31 21:29:36 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,12 @@
 import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-// import HomePage from 'pages/HomePage';
-// import MoviesPage from 'pages/MoviesPage';
-// import ErrorPage from 'pages/ErrorPage';
 import SharedLayout from 'Layout/SharedLayout';
-// import MoviesDetails from './MoviesDetails/MoviesDetails';
-// import Cast from './Cast/Cast';
-// import Reviews from './Reviews/Reviews';
 
 const HomePage = lazy(() => import('pages/HomePage'));
 const MoviesPage = lazy(() => import('pages/MoviesPage'));
 const ErrorPage = lazy(() => import('pages/ErrorPage'));
-// const SharedLayout = lazy(() => import('Layout/SharedLayout'));
+
 const MoviesDetails = lazy(() => import('./MoviesDetails/MoviesDetails'));
 const Cast = lazy(() => import('./Cast/Cast'));
 const Reviews = lazy(() => import('./Reviews/Reviews'));
