f2243f4c877a37b60b2ff125b997822d472fc680
SirotovAlexander <alexander.sirotov@gmail.com>
2023-07-31 18:37:41 +0300

    done styles
    done styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,16 @@
 import { NavLink } from 'react-router-dom';
+import css from './Header.module.css';
 
 const Header = () => {
   return (
-    <header>
-      <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/movies">Movies</NavLink>
+    <header className={css.header__wrapper}>
+      <nav className={css.header__navigation}>
+        <NavLink className={css.header__navlink} to="/">
+          Home
+        </NavLink>
+        <NavLink className={css.header__navlink} to="/movies">
+          Movies
+        </NavLink>
       </nav>
     </header>
   );
