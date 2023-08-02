6315baa8053dcf7d86ff6238440fa9ed3ff96b7c
SirotovAlexander <116684295+SirotovAlexander@users.noreply.github.com>
2023-08-02 15:03:05 +0000

    Deploying to gh-pages from @ SirotovAlexander/goit-react-hw-05-movies@d79994266249c250b823c3e3734f87da875ac92a 🚀

---
+++ static/js/811.4ad1f05f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[811],{811:function(r,e,n){n.r(e),n.d(e,{default:function(){return i}});var t="ErrorPage_main__n4Zv9",a="ErrorPage_fof__mSYH1",c=n(184),i=function(){return(0,c.jsx)("div",{id:t,children:(0,c.jsx)("div",{className:a,children:(0,c.jsx)("h1",{children:"Error 404"})})})}}}]);
+//# sourceMappingURL=811.4ad1f05f.chunk.js.map
